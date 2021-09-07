6e848c5027800addaad01cd6d89162a38227c6af
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-09-08 00:14:59 +0700

    feat: pooling layer, refactored layers

---
+++ cnn/layer/__init__.py
@@ -0,0 +1,2 @@
+from .base import BaseLayer
+from .convolutional import ConvolutionalLayer
---
+++ cnn/layer/base.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class BaseLayer:
+    """
+    Defines base class for a layer.
+    """
+
+    def __init__(self):
+        return
+
+    def run(self, inputs: np.array):
+        return inputs
