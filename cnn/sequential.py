e01d04c19161234a9ccc51040c36190a16aecb7d
FaridLazuarda <faridadilazuarda29@gmail.com>
2021-09-20 20:30:33 +0700

    feat: fix log loss

--- cnn/sequential.py
+++ cnn/sequential.py
@@ -34,6 +34,24 @@ class Sequential:
 
         return np.array(final_result)
 
+    def ll(self, actual, predicted):
+        actual = np.array(actual)
+        predicted = np.array(predicted)
+        for i in range(0, predicted.shape[0]):
+            predicted[i] = min(max(1e-15, predicted[i]), 1 - 1e-15)
+        err = np.seterr(all="ignore")
+        score = -(actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted))
+        np.seterr(divide=err["divide"], over=err["over"], under=err["under"], invalid=err["invalid"])
+        if isinstance(score, np.ndarray):
+            score[np.isnan(score)] = 0
+        else:
+            if np.isnan(score):
+                score = 0
+        return score
+
+    def log_loss(self, actual, predicted):
+        return np.mean(self.ll(actual, predicted))
+
     def mean_squared_error(self, actual, predicted):
         sum_square_error = 0.0
         for i in range(len(actual)):
