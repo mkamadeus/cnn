fix: change dense compute delta behavior if softmax

cnn/activations.py
@@ -34,5 +34,5 @@ def softmax_derivative(output: np.ndarray, target_class: int):
     This is actually the derivative of negative log likelihood loss and softmax activation function.
     """
     target_arr = np.zeros(output.shape)
-    target_arr[target_class][0] = 1.0
+    target_arr[target_class] = 1.0
     return output - target_arr

cnn/layer/dense.py
@@ -39,6 +39,7 @@ class Dense(BaseLayer):
         biased_input: np.ndarray = np.insert(inputs, 0, 1)
         ic(inputs)
         biased_input = np.expand_dims(biased_input, axis=1)
+        ic(biased_input)
 
         result = np.matmul(self.weights.T, biased_input).flatten()
         ic(result)
@@ -82,14 +83,18 @@ class Dense(BaseLayer):
         # TODO: handle softmax
 
         # make delta = dE/dout * dout/dnet using element wise mmult
-        delta *= derivative_activation_function(self.output)
+        if self.activation in ["relu", "sigmoid", "linear"]:
+            delta *= derivative_activation_function(self.output)
         # ic(derivative_activation_function(delta))
 
         ic(delta, self.input, self.activated_output, self.weights)
 
         # add bias to input
         biased_input: np.ndarray = np.insert(self.input, 0, 1)
+        ic(biased_input)
+        ic(delta)
         self.delta = np.matmul(np.array([biased_input]).T, np.array([delta]))
+        ic(self.delta)
 
         # store deltas for bias and weight
         self.delta_bias = self.delta[0]
@@ -102,8 +107,8 @@ class Dense(BaseLayer):
 
         # calculate delta for prev layer
         # ini buat apa ya wkwkwk
-        delta_input = np.matmul(np.array([delta]), self.weights[1:].T)
-        ic(delta_input)
+        # delta_input = np.matmul(np.array([delta]), self.weights[1:].T)
+        # ic(delta_input)
 
         # # TODO: verify the truthiness of this formula.. not really sure lol
         # # set derivative of activation function

tests/unit/test_dense.py
@@ -1,5 +1,7 @@
 import numpy as np
 from cnn.layer import Dense
+from cnn.activations import softmax_derivative
+from icecream import ic
 
 
 def test_dense():
@@ -14,10 +16,60 @@ def test_dense():
     assert np.testing.assert_array_almost_equal(result, expected, decimal=6) is None
 
 
-def test_backprop_dense():
+def test_backprop_dense_1():
     layer = Dense(size=2, input_size=2, weights=np.array([[0, 0], [1, 2], [3, -4]]), activation="relu")
     layer.run(inputs=np.array([118, 102]))
     result = layer.compute_delta(np.array([24.0, 10.0]))
     expected = np.array([[2832.0, 0.0], [2448.0, 0.0]])
 
     assert np.testing.assert_array_almost_equal(result, expected) is None
+
+
+def test_backprop_dense_2():
+    layer = Dense(
+        size=10,
+        input_size=2,
+        weights=np.array(
+            [
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.04, 0.05, 0.01],
+                [0.02, 0.03, 0.02, 0.02, 0.01, 0.02, 0.07, 0.08, 0.05, 0.01],
+            ]
+        ),
+        activation="softmax",
+    )
+    layer.run(inputs=np.array([424, 0]))
+    first_delta = softmax_derivative(
+        output=np.array(
+            [
+                9.855924e-01,
+                1.420001e-02,
+                2.045880e-04,
+                2.947620e-06,
+                4.246811e-08,
+                6.118632e-10,
+                8.815475e-12,
+                6.118632e-10,
+                4.246811e-08,
+                1.829905e-15,
+            ]
+        ),
+        target_class=9,
+    )
+    result = layer.compute_delta(first_delta)
+    ic(first_delta)
+    expected = np.array(
+        [
+            [4.18e02, 0.00e00],
+            [6.02e00, 0.00e00],
+            [8.67e-02, 0.00e00],
+            [1.25e-03, 0.00e00],
+            [1.80e-05, 0.00e00],
+            [2.59e-07, 0.00e00],
+            [3.74e-09, 0.00e00],
+            [2.59e-07, 0.00e00],
+            [1.80e-05, 0.00e00],
+            [-4.24e02, 0.00e00],
+        ]
+    ).T
+    assert np.testing.assert_allclose(result, expected, rtol=1e-02) is None