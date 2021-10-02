feat: backprop max kinda fixed?

cnn/layer/pooling/max_pooling.py
@@ -4,26 +4,20 @@ from typing import Tuple
 from cnn.layer.base import BaseLayer
 from icecream import ic
 
-POOLING_MODES = ["max", "average"]
-
 
 def max_pooling_mask(view: np.ndarray, value: float, delta_value: float):
     result = np.array([[delta_value if col == value else 0 for col in row] for row in view])
     return result
 
 
-class Pooling(BaseLayer):
+class MaxPooling(BaseLayer):
     """
     Defines a pooling layer consisting of inputs and kernels.
     """
 
-    def __init__(self, size, stride, mode="average"):
-        if mode not in POOLING_MODES:
-            raise Exception("invalid pooling mode")
-
+    def __init__(self, size, stride):
         self.size: Tuple[int, int] = size
         self.stride: int = stride
-        self.mode: str = mode
         self.type = "pooling      "
 
     def run_pooling(self, inputs):
@@ -33,10 +27,7 @@ class Pooling(BaseLayer):
             strided_views = generate_strides(i, self.size, stride=self.stride)
 
             # make feature map
-            if self.mode == "average":
-                feature_map = np.array([[np.average(view) for view in row] for row in strided_views])
-            elif self.mode == "max":
-                feature_map = np.array([[np.max(view) for view in row] for row in strided_views])
+            feature_map = np.array([[np.max(view) for view in row] for row in strided_views])
 
             result.append(feature_map)
         return np.array(result)
@@ -57,16 +48,26 @@ class Pooling(BaseLayer):
         ic(delta, self.input, self.output)
 
         result = np.zeros(self.input.shape)
-        for idx, err in enumerate(self.input):
+        for idx in range(len(self.input)):
             for i in range(delta[idx].shape[0]):
-                for j in range(delta[idx].shape[0]):
-                    max_value = self.output[idx, i, j]
-                    err_value = delta[idx, i, j]
-                    ic(max_value, err_value)
-                    input_view = self.input[idx, i : i + self.size[0], j : j + self.size[1]]
-                    input_view = np.where(input_view == max_value, err_value, 0)
-                    ic(input_view)
-                    result[idx, i : i + self.size[0], j : j + self.size[1]] += input_view
+                if i % self.stride == 0:
+                    for j in range(delta[idx].shape[0]):
+                        if j % self.stride == 0:
+                            max_value = self.output[idx, i, j]
+                            err_value = delta[idx, i, j]
+                            ic(max_value, err_value)
+
+                            input_view = np.zeros(self.size).flatten()
+                            for k, input_value in enumerate(
+                                self.input[idx, i : i + self.size[0], j : j + self.size[1]].flatten()
+                            ):
+                                if input_value == max_value:
+                                    input_view[k] = err_value
+                                    break
+                            input_view = input_view.reshape(self.size)
+                            ic(input_view)
+
+                            result[idx, i : i + self.size[0], j : j + self.size[1]] += input_view
 
         ic(result)
         return result

tests/unit/test_pooling.py
@@ -1,45 +1,41 @@
-from cnn.layer import Pooling
+from cnn.layer.pooling import MaxPooling
 import pytest
 import numpy as np
 
 
 @pytest.mark.parametrize(
-    "pool_input,pool_output,size,stride,mode",
+    "pool_input,pool_output,size,stride",
     [
-        (np.arange(1, 17).reshape((1, 4, 4)), np.array([[[6, 7, 8], [10, 11, 12], [14, 15, 16]]]), (2, 2), 1, "max"),
+        (np.arange(1, 17).reshape((1, 4, 4)), np.array([[[6, 7, 8], [10, 11, 12], [14, 15, 16]]]), (2, 2), 1),
         (
             np.array([[[1, 1, 1, 1], [1, 99, 99, 1], [1, 99, 99, 1], [1, 1, 1, 1]]]),
             np.array([[[99, 99], [99, 99]]]),
             (3, 3),
             1,
-            "max",
         ),
         (
             np.arange(1, 17).reshape((1, 4, 4)),
             np.array([[[3.5, 4.5, 5.5], [7.5, 8.5, 9.5], [11.5, 12.5, 13.5]]]),
             (2, 2),
             1,
-            "average",
         ),
         (
             np.array([[[1, 1, 1, 1], [1, 99, 99, 1], [1, 99, 99, 1], [1, 1, 1, 1]]]),
             np.array([[[44.555556, 44.555556], [44.555556, 44.555556]]]),
             (3, 3),
             1,
-            "average",
         ),
     ],
 )
-def test_pooling_forward(pool_input, pool_output, size, stride, mode):
-    print(pool_input)
-    layer = Pooling(size=size, stride=stride, mode=mode)
+def test_pooling_forward(pool_input, pool_output, size, stride):
+    layer = MaxPooling(size=size, stride=stride)
     result = layer.run(pool_input)
     assert np.testing.assert_array_almost_equal(result, pool_output) is None
     pass
 
 
 @pytest.mark.parametrize(
-    "pool_input,pool_output,delta_input,delta_output,size,stride,mode",
+    "pool_input,pool_output,delta_input,delta_output,size,stride",
     [
         (
             np.arange(1, 17).reshape((1, 4, 4)),
@@ -57,13 +53,28 @@ def test_pooling_forward(pool_input, pool_output, size, stride, mode):
             ),
             (2, 2),
             1,
-            "max",
+        ),
+        (
+            np.array([[[1, 1, 1, 1], [1, 99, 99, 1], [1, 99, 99, 1], [1, 1, 1, 1]]]),
+            np.array([[[99, 99], [99, 99]]]),
+            np.array([[[0.1, 0.2], [0.3, 0.4]]]),
+            np.array(
+                [
+                    [
+                        [0, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                    ]
+                ]
+            ),
+            (3, 3),
+            1,
         ),
     ],
 )
-def test_pooling_backward(pool_input, pool_output, delta_input, delta_output, size, stride, mode):
-    print(pool_input)
-    layer = Pooling(size=size, stride=stride, mode=mode)
+def test_pooling_backward(pool_input, pool_output, delta_input, delta_output, size, stride):
+    layer = MaxPooling(size=size, stride=stride)
     layer.input = pool_input
     layer.output = pool_output
 