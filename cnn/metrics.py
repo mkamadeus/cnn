59f2770143de044d30ce14794cf3fff5154fd04b
stefanusgusega <gunawanstefanus006@gmail.com>
2021-10-08 19:45:44 +0700

    fix: formatting with black

--- cnn/metrics.py
+++ cnn/metrics.py
@@ -1,14 +1,20 @@
 import numpy as np
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 
 
 def compute_tp_tn_fn_fp(y_true, y_pred):
-    '''
+    """
     True positive - actual = 1, predicted = 1
     False positive - actual = 1, predicted = 0
     False negative - actual = 0, predicted = 1
     True negative - actual = 0, predicted = 0
-    '''
+    """
     tp = sum((y_true == 1) & (y_pred == 1))
     tn = sum((y_true == 0) & (y_pred == 0))
     fn = sum((y_true == 1) & (y_pred == 0))
@@ -17,40 +23,38 @@ def compute_tp_tn_fn_fp(y_true, y_pred):
 
 
 def acc_score(y_true, y_pred):
-    '''
+    """
     Accuracy = TP + TN / FP + FN + TP + TN
-    '''
-    tp,tn,fp,fn = compute_tp_tn_fn_fp(y_true,y_pred)
-    return (tp + tn)/float(tp + tn + fn + fp)
+    """
+    tp, tn, fp, fn = compute_tp_tn_fn_fp(y_true, y_pred)
+    return (tp + tn) / float(tp + tn + fn + fp)
 
 
 def prec_score(y_true, y_pred):
-    '''
-    Precision = TP  / FP + TP 
-    '''
-    tp,tn,fp,fn = compute_tp_tn_fn_fp(y_true,y_pred)
-    return tp/float(tp + fp)
+    """
+    Precision = TP  / FP + TP
+    """
+    tp, tn, fp, fn = compute_tp_tn_fn_fp(y_true, y_pred)
+    return tp / float(tp + fp)
 
 
 def rec_score(y_true, y_pred):
-    '''
-    Recall = TP /FN + TP 
-    '''
-    tp,tn,fp,fn = compute_tp_tn_fn_fp(y_true,y_pred)
-    return tp/float(tp + fn)
+    """
+    Recall = TP /FN + TP
+    """
+    tp, tn, fp, fn = compute_tp_tn_fn_fp(y_true, y_pred)
+    return tp / float(tp + fn)
 
 
 def f1(y_true, y_pred):
-    '''
+    """
     F1 = 2*((precision * recall)/(precision + recall))
     F1 = TP/(TP+ 0.5* (FP + FN))
-    '''
+    """
     precision = prec_score(y_true, y_pred)
     recall = rec_score(y_true, y_pred)
-    f1_score = (2  * (precision * recall)/(precision + recall))
+    f1_score = 2 * (precision * recall) / (precision + recall)
     return f1_score
-
-
 
 
 y_t = np.array([1, 1, 1, 1, 1, 0, 1, 0, 0, 1])
