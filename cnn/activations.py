fix: softmax

cnn/activations.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 # from numpy import float128
 
 
@@ -28,8 +29,9 @@ def relu_derivative(x: np.ndarray):
 
 
 def softmax(layer: np.ndarray):
-    f = np.exp(layer - np.max(layer))
-    return lambda x: np.exp(x) / np.sum(np.exp(f))
+    # f = layer - np.max(layer)
+    return lambda x: np.exp(x - np.max(layer)) / np.sum(np.exp(x - np.max(layer)))
+
 
 def softmax_derivative(output: np.ndarray, target_class: int):
     """

tests/unit/test_cnn_activations.py
@@ -87,7 +87,7 @@ def test_softmax():
 
     softmax_fun = softmax(layer)
 
-    assert np.sum(softmax_fun(layer)) == 1.0
+    assert np.testing.assert_allclose(np.sum(softmax_fun(layer)), 1.0) is None
 
 
 def test_softmax_derivative():