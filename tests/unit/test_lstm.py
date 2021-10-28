6a70c76d5cbb162e4c9ffdfece749edef8668dac
stefanusgusega <gunawanstefanus006@gmail.com>
2021-10-29 05:39:11 +0700

    fix: modify run return + weight count + test

--- tests/unit/test_lstm.py
+++ tests/unit/test_lstm.py
@@ -1,17 +1,12 @@
 import numpy as np
 from cnn.layer import LSTM
-from cnn.activations import softmax_derivative
 from icecream import ic
 
 
-def test_dense():
-    layer = LSTM(
-        size=3,
-        input_size=(2,2)
-    )
+def test_lstm():
+    layer = LSTM(size=1, input_size=(2, 2))
     result = layer.run(inputs=np.array([[1, 2], [0.5, 3]]))
-    print(result)
-    expected = np.array([1.517633098])
+    ic(result)
+    expected = np.array([0.7719811058])
 
     assert np.testing.assert_array_almost_equal(result, expected, decimal=6) is None
-
