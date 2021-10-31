a041223abf691dbe237227f31c921ef335fd2a86
stefanusgusega <gunawanstefanus006@gmail.com>
2021-10-31 22:54:44 +0700

    feat: center all gates attribute into a variable

--- tests/unit/test_lstm.py
+++ tests/unit/test_lstm.py
@@ -7,6 +7,6 @@ def test_lstm():
     layer = LSTM(size=1, input_size=(2, 2))
     result = layer.run(inputs=np.array([[1, 2], [0.5, 3]]))
     ic(result)
-    expected = np.array([0.7719811058])
+    expected = np.array([[0.7719811058]])
 
     assert np.testing.assert_array_almost_equal(result, expected, decimal=6) is None
